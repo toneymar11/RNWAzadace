679a8ed4277fe1cb94b94e1463bafdc8622d3dd7
toneymar11 <tonykopilas11@gmail.com>
2020-09-29 21:56:01 +0200

    dodan mvc projektni dio s autentifikacijom

---
+++ MVC-Northwind/Areas/Identity/Data/MVC_NorthwindContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Northwind.Data
+{
+    public class MVC_NorthwindContext : IdentityDbContext<IdentityUser>
+    {
+        public MVC_NorthwindContext(DbContextOptions<MVC_NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ MVC-Northwind/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_Northwind.Data;
+
+[assembly: HostingStartup(typeof(MVC_Northwind.Areas.Identity.IdentityHostingStartup))]
+namespace MVC_Northwind.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MVC_NorthwindContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MVC_NorthwindContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MVC_NorthwindContext>();
+            });
+        }
+    }
+}
